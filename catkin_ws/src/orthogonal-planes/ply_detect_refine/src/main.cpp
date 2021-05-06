a5c4bd9e4b6d7247052f999e7465a1d98d55f835
Alexpop104 <alexandrupop104@gmail.com>
2021-05-06 18:16:45 +0300

    Can see line points

--- catkin_ws/src/orthogonal-planes/ply_detect_refine/src/main.cpp
+++ catkin_ws/src/orthogonal-planes/ply_detect_refine/src/main.cpp
@@ -1,4 +1,4 @@
-/**
+/* /**
 BSD 3-Clause License
 
 This file is part of the code accompanying the paper
@@ -77,6 +77,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
+
+
 class LineDetectNode
 {
 public:
@@ -85,7 +87,7 @@ public:
 
   LineDetectNode()
   {
-    pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/point_final",1);
+    pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/lines_all",1);
     sub_ = nh_.subscribe ("/norm_out", 1,  &LineDetectNode::cloudCallback, this);
     config_server_.setCallback(boost::bind(&LineDetectNode::dynReconfCallback, this, _1, _2));
   }
@@ -121,7 +123,10 @@ public:
     std::vector<Eigen::Vector3f> points;
     std::vector<Eigen::Vector3f> normals;
 
+    if(cloudPTR->size()>0)
+    {
 
+    
 
     for (int i = 0; i < cloudPTR->size(); i++)
     {
@@ -160,6 +165,12 @@ public:
     planeMap.find_triangles(triangles);
         
     // // reduce graph to ParallelPlaneGraph
+
+    if (triangles.size()==0){
+       std::cout<<"Nu sunt triunghiuri"<<'\n'; 
+    }
+
+    
     
     ParallelPlaneGraph redMap = planeMap.reduce_graph();
     
@@ -192,19 +203,19 @@ public:
        std::vector<Vec6> lines;
 // //    // line extraction and visualization
  
-    lines = finalMap.extract_lines(points, normals);
+       lines = finalMap.extract_lines(points, normals);
    
-    std::cout << lines.size() << " lines found." << std::endl;
+      std::cout << lines.size() << " lines found." << std::endl;
 
-   std::vector<Vec6> new_lines;
+     std::vector<Vec6> new_lines;
 
-   std::vector<Vec6> final_lines;
-   std::vector<double> distances;
+     std::vector<Vec6> final_lines;
+     std::vector<double> distances;
 
-   std::vector<double> Volume_edge;
+     std::vector<double> Volume_edge;
 
-   std::vector<int> positions_wrong;
-   std::vector<double> Volumes;
+     std::vector<int> positions_wrong;
+     std::vector<double> Volumes;
 
    
 
@@ -336,13 +347,19 @@ public:
       }
 
      
-    if(new_lines.size()>=3){
+    if (new_lines.size()>=3){
        for(int i = 0; i < new_lines.size(); i=i+3) {
       bool ok_position=1;
+
+      
+      if(positions_wrong.size()>0){
       for(int j = 0; j != positions_wrong.size(); j++) {
         if(i==positions_wrong[j]){
           ok_position=0;
         }
+      
+      
+      }
       }
       if(ok_position){
         final_lines.push_back(new_lines[i]);
@@ -359,27 +376,70 @@ public:
         std::cout<<'\n';
 
       }
-    }
+       }
+      }
 
-    }
+
+
+  
   
 
-   
+  
+    
+  
+ }
+
+ pcl::PointCloud<pcl::PointXYZ>::Ptr points_all_lines(new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::PointXYZ p;
+  if(lines.size()>0){
+    for(int i = 0; i < lines.size(); i=i+3) {
+      p.x = lines[i][0];
+      p.y = lines[i][1];
+      p.z = lines[i][2];
+
+      points_all_lines->points.push_back(p);
+
+       p.x = lines[i][3];
+       p.y = lines[i][4];
+       p.z = lines[i][5];
+
+      points_all_lines->points.push_back(p);
+  }
+
+   points_all_lines->width = points_all_lines->points.size();
+   points_all_lines->height = 1;
+   points_all_lines->points.resize(points_all_lines->width * points_all_lines->height);
+   points_all_lines->is_dense = false;
+
+  }
+
+   std::stringstream header_camera;
+    header_camera << "camera_depth_optical_frame";
+
+    sensor_msgs::PointCloud2 tempROSMsg;
+    pcl::toROSMsg(*points_all_lines, tempROSMsg);
+
+    
+    tempROSMsg.header.frame_id = header_camera.str();
+    pub_.publish(tempROSMsg);
+
+
+    
+       
 
  } 
 
    
 
 
-    // for(int i = 0; i != new_lines.size(); i++) {
-    // //std::cout<< new_lines[i][0]<<" "<<new_lines[i][1]<<" "<<new_lines[i][2]<<" "<<new_lines[i][3]<<" "<<new_lines[i][4]<<" "<<new_lines[i][5]<<" "<<'\n';
-    // }   
+   
     
 
     
 
 
-  }
+}
 
 private:
   ros::NodeHandle nh_;
@@ -408,271 +468,7 @@ int main(int argc, char** argv) {
 
    ros::spin();
 
-    Timer T;
-    
-    
 
-
-
-
-    
-
-//     // reduce graph to ParallelPlaneGraph
-//     T.tic();
-//     ParallelPlaneGraph redMap = planeMap.reduce_graph();
-//     T.toc("graph reduction");
-//     //redMap.print_info();
-//     //redMap.print_edges();
-//     T.tic();
-//     redMap.triangle_reduce();
-//     T.toc("triangle reduction");
-//     //redMap.print_info();
-//     //redMap.print_parameters();
-//     //redMap.print_edges();
-    
-//     T.tic();
-//     redMap.filter_outliers(points, normals, pairDet.distance_bin(), .5*sampling);
-//     T.toc("outlier filtering");
-//     //redMap.print_info();
-//     //redMap.print_parameters();
-//     //redMap.print_edges();
- 
-//     // CERES problem setup and solving
-//     double lambda = 0.01 * points.size();
-//     T.tic();
-//     redMap.refine_coarse_fine(points, normals, lambda, pairDet.distance_bin(), sampling);
-//     T.toc("Multi-plane parameter optimization");
-//     //redMap.print_info();
-//     //redMap.print_parameters();
-//     //redMap.print_edges();
-    
-//     T.tic();
-//     redMap.filter_outliers(points, normals, pairDet.distance_bin(), .5*sampling);
-//     T.toc("outlier filtering");
-//     //redMap.print_info();
-//     //redMap.print_parameters();
-//     //redMap.print_edges();
-    
-//     // From here on visualization / debugging
-
-// //    // get points located on planes and not-planes
-//     PlaneGraph finalMap = PlaneGraph::from_ppg(redMap, 2*pairDet.para_thresh()*pairDet.para_thresh()-1, pairDet.distance_bin()); // double angle
-//     std::vector<Eigen::Matrix<float, 6, 1>> labeled;
-//     T.tic();
-//     finalMap.go_thr_pcl(points, normals, labeled);
-//     T.toc("labeling planes");
-
-//     // visualize
-//    // pcshow(labeled, "Result");
-    
-//    std::vector<Vec6> lines;
-//    // line extraction and visualization
-//    T.tic();
-//    lines = finalMap.extract_lines(points, normals);
-//    T.toc("get lines");
-//    //std::cout << lines.size() << " lines found." << std::endl;
-
-//    std::vector<Vec6> new_lines;
-
-//    std::vector<Vec6> final_lines;
-//    std::vector<double> distances;
-
-//    std::vector<double> Volume_edge;
-
-//    std::vector<int> positions_wrong;
-//    std::vector<double> Volumes;
-
-
-//    /*for (std::vector<Vec6>::const_iterator i = lines.begin(); i != lines.end(); ++i){
-//       std::cout << *i << ' ';
-//    }*/
-
-//    /*for (const auto l : lines) {
-//         const double cols[]  = { 1,0,0, 1,0,0 };
-//         const double verts[] = { l[0], l[1], l[2], l[3], l[4], l[5] };
-
-//         std::cout<<l[0]<<" "<<l[1]<<" "<<l[2]<<" "<<l[3]<<" "<<l[4]<<" "<<l[5]<<'\n';
-//         //glLineWidth(5.);
-//         //pangolin::glDrawColoredVertices<double, double>(2, verts, cols, GL_LINES, 3, 3);
-//     }*/
-   
-
-//     std::cout<<'\n';
-
-    
-
-//     for(int i = 0; i != lines.size()-2; i++) {
-//     //std::cout<< lines[i][0]<<" "<<lines[i][1]<<" "<<lines[i][2]<<" "<<lines[i][3]<<" "<<lines[i][4]<<" "<<lines[i][5]<<" "<<'\n';
-//       for(int j = i+1; j != lines.size()-1; j++) {
-//           for(int k = j+1; k != lines.size(); k++) {
-
-//                 bool ok_ij=1;
-//                 bool ok_jk=1;
-//                 bool ok_ki=1;
-
-//                 double average_0,average_1,average_2;
-
-                
-//                 if( (lines[i][0]==lines[j][0]) && (lines[i][1]==lines[j][1]) && (lines[i][2]==lines[j][2])) {ok_ij=0;}
-//                 if( (lines[j][0]==lines[k][0]) && (lines[j][1]==lines[k][1]) && (lines[j][2]==lines[k][2])) {ok_jk=0;}
-//                 if( (lines[k][0]==lines[i][0]) && (lines[k][1]==lines[i][1]) && (lines[k][2]==lines[i][2])) {ok_ki=0;}
-                
-//                 if (ok_ij && ok_jk && ok_ki){
-                  
-//               average_0=(lines[i][0]+lines[j][0]+lines[k][0]) / 3;
-//               average_1=(lines[i][1]+lines[j][1]+lines[k][1]) /3 ;
-//               average_2=(lines[i][2]+lines[j][2]+lines[k][2]) /3;
-
-
-//               double dist_i_average= sqrt (  (lines[i][0]-average_0)*(lines[i][0]-average_0)  +  (lines[i][1]-average_1)*(lines[i][1]-average_1) +(lines[i][2]-average_2)*(lines[i][2]-average_2)   );
-//               double dist_j_average= sqrt (  (lines[j][0]-average_0)*(lines[j][0]-average_0)  +  (lines[j][1]-average_1)*(lines[j][1]-average_1) +(lines[j][2]-average_2)*(lines[j][2]-average_2)   );
-//               double dist_k_average= sqrt (  (lines[k][0]-average_0)*(lines[k][0]-average_0)  +  (lines[k][1]-average_1)*(lines[k][1]-average_1) +(lines[k][2]-average_2)*(lines[k][2]-average_2)   );
-
-
-//               if(  (dist_i_average<threshold) && (dist_j_average<threshold) &&(dist_k_average<threshold) )
-//               {
-//                   /*
-//                   std::cout<< lines[i][0]<<" "<<lines[i][1]<<" "<<lines[i][2]<<" "<<'\n';      
-//                   std::cout<< lines[j][0]<<" "<<lines[j][1]<<" "<<lines[j][2]<<" "<<'\n';
-//                   std::cout<< lines[k][0]<<" "<<lines[k][1]<<" "<<lines[k][2]<<" "<<'\n';
-//                   */
-
-//                   distances.push_back(dist_i_average);
-//                   distances.push_back(dist_j_average);
-//                   distances.push_back(dist_k_average);
-                  
-
-//                   /* 
-//                   std::cout<<"Average:"<<average_0<<" "<<average_1<<" "<<average_2<<"\n";
-//                   std::cout<<"\n";
-//                   */
-//                   /*
-//                 std::cout<< lines[i][0]<<" "<<lines[i][1]<<" "<<lines[i][2]<<" "<<lines[i][3]<<" "<<lines[i][4]<<" "<<lines[i][5]<<" "<<'\n';
-//                 std::cout<< lines[j][0]<<" "<<lines[j][1]<<" "<<lines[j][2]<<" "<<lines[j][3]<<" "<<lines[j][4]<<" "<<lines[j][5]<<" "<<'\n';
-//                 std::cout<< lines[k][0]<<" "<<lines[k][1]<<" "<<lines[k][2]<<" "<<lines[k][3]<<" "<<lines[k][4]<<" "<<lines[k][5]<<" "<<'\n';
-//                 */
-//                 double latura_i = sqrt (  (lines[i][0]-lines[i][3])*(lines[i][0]-lines[i][3])  +  (lines[i][1]-lines[i][4])*(lines[i][1]-lines[i][4]) +(lines[i][2]-lines[i][5])*(lines[i][2]-lines[i][5])   );
-//                 double latura_j = sqrt (  (lines[j][0]-lines[j][3])*(lines[j][0]-lines[j][3])  +  (lines[j][1]-lines[j][4])*(lines[j][1]-lines[j][4]) +(lines[j][2]-lines[j][5])*(lines[j][2]-lines[j][5])   );
-//                 double latura_k = sqrt (  (lines[k][0]-lines[k][3])*(lines[k][0]-lines[k][3])  +  (lines[k][1]-lines[k][4])*(lines[k][1]-lines[k][4]) +(lines[k][2]-lines[k][5])*(lines[k][2]-lines[k][5])   );
-
-//                 double Volum = latura_i*latura_j*latura_k;
-//                  /*
-//                 std::cout<<"Latura 1: "<<latura_1<<'\n';
-//                 std::cout<<"Latura 2: "<<latura_3<<'\n';
-//                 std::cout<<"Latura 3: "<<latura_3<<'\n';
-//                 */
-
-//                 //std::cout<<"Volum="<<Volum<<'\n';
-
-//                 new_lines.push_back(lines[i]);
-//                 new_lines.push_back(lines[j]);
-//                 new_lines.push_back(lines[k]);
-
-//                 Volume_edge.push_back(latura_i);
-//                 Volume_edge.push_back(latura_j);
-//                 Volume_edge.push_back(latura_k);
-
-//                 Volumes.push_back(Volum);
-//                 Volumes.push_back(Volum);
-//                 Volumes.push_back(Volum);
-//               }
-
-//               }
-//             }     
-//           }
-
-//        }
-
-
-//     for(int i = 0; i != new_lines.size(); i++) {
-//     //std::cout<< new_lines[i][0]<<" "<<new_lines[i][1]<<" "<<new_lines[i][2]<<" "<<new_lines[i][3]<<" "<<new_lines[i][4]<<" "<<new_lines[i][5]<<" "<<'\n';
-//     }   
-    
-
-//     for(int i = 0; i != new_lines.size()-1; i++) {
-    
-//       for(int j = i+1; j != new_lines.size(); j++) {
-//           if( ( (new_lines[i][0]==new_lines[j][0])  && (new_lines[i][1]==new_lines[j][1]) && (new_lines[i][2]==new_lines[j][2]) && (new_lines[i][3]==new_lines[j][3]) && (new_lines[i][4]==new_lines[j][4]) && (new_lines[i][5]==new_lines[j][5]) ) ||
-//               ( (new_lines[i][0]==new_lines[j][3])  && (new_lines[i][1]==new_lines[j][4])&&  (new_lines[i][2]==new_lines[j][5]) && (new_lines[i][3]==new_lines[j][0]) && (new_lines[i][4]==new_lines[j][1]) && (new_lines[i][5]==new_lines[j][2]) ) )  
-//                {
-//             //std::cout<<"Dublura gasita la"<<i<<" "<<j<<'\n';
-
-//             double total_distance_i;
-//             double total_distance_j;
-            
-//             int offset_i= (i % 3);
-//             int offset_j= (j % 3);
-
-            
-
-//             total_distance_i=distances[i - offset_i]+distances[i+1 - offset_i]+distances[i+2 - offset_i];
-//             total_distance_j=distances[j - offset_j]+distances[j+1 - offset_j]+distances[j+2 - offset_j];
-
-//             //std::cout<<"Distance i: "<<total_distance_i<<"\n";
-//            // std::cout<<"Distance j: "<<total_distance_j<<"\n";
-
-//             if(total_distance_i>total_distance_j){
-//               positions_wrong.push_back(i - offset_i);
-//             }
-//             else
-//             {
-//                positions_wrong.push_back(j - offset_j);
-//             }
-
-            
-//           }
-          
-//       }
-//     }
-  
-//   /*
-//   std::cout<<"Positions wrong:"<<'\n';
-
-
-//   for(int i = 0; i != positions_wrong.size(); i++) {
-//     std::cout<<positions_wrong[i]<<'\n';
-//     }   
-
-//     */
-
-//     for(int i = 0; i < new_lines.size(); i=i+3) {
-//       bool ok_position=1;
-//       for(int j = 0; j != positions_wrong.size(); j++) {
-//         if(i==positions_wrong[j]){
-//           ok_position=0;
-//         }
-//       }
-//       if(ok_position){
-//         final_lines.push_back(new_lines[i]);
-//         final_lines.push_back(new_lines[i+1]);
-//         final_lines.push_back(new_lines[i+2]);
-//         std::cout<<"Volum "<<(i/3)<<": "<<Volumes[i]<<" cm^3"<<'\n';
-
-//         /*
-//         std::cout<< "("<<new_lines[i][0]<<" , "<<new_lines[i][1]<<" , "<<new_lines[i][2]<<" , "<<new_lines[i][3]<<" , "<<new_lines[i][4]<<" , "<<new_lines[i][5]<<")"<<" =>distance1= "<<Volume_edge[i] <<'\n';
-//         std::cout<< "("<<new_lines[i+1][0]<<" , "<<new_lines[i+1][1]<<" , "<<new_lines[i+1][2]<<" , "<<new_lines[i+1][3]<<" , "<<new_lines[i+1][4]<<" , "<<new_lines[i+1][5]<<")"<<" =>distance2= "<<Volume_edge[i+1] <<'\n';
-//         std::cout<< "("<<new_lines[i+2][0]<<" , "<<new_lines[i+2][1]<<" , "<<new_lines[i+2][2]<<" , "<<new_lines[i+2][3]<<" , "<<new_lines[i+2][4]<<" , "<<new_lines[i+2][5]<<")"<<" =>distance3= "<<Volume_edge[i+2] <<'\n';
-//         */
-
-//         std::cout<< "distance1= "<<Volume_edge[i] <<" cm"<<'\n';
-//         std::cout<< "distance2= "<<Volume_edge[i+1]<<" cm" <<'\n';
-//         std::cout<< "distance3= "<<Volume_edge[i+2]<<" cm" <<'\n';
-        
-//         std::cout<<'\n';
-
-//       }
-//     }
-
-     
-  
-   
-   //pcshow_lines(points, lines, "Orth Plane Intersections");
-
-  //  pcshow_lines(points, new_lines, "Volume Computation Edges");
-
-  //  pcshow_lines(points, final_lines, "Volume Final Edges");
-
-
- 
     
 }
+ 
